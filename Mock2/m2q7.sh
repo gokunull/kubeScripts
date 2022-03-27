1fbe4c612c2df85a98458a02e0dcc07e7a4b0713
gokunull <anon.hkk@gmail.com>
2022-03-27 12:46:30 +0300

    edit q 7

--- Mock2/m2q7.sh
+++ Mock2/m2q7.sh
@@ -7,8 +7,8 @@ $k run nginx-resolver --image=nginx
 
 $k expose pod nginx-resolver --name=nginx-resolver-service --port=80 --target-port=80 --type=ClusterIP
 
-PODIP=$(k describe po nginx-resolver | grep -i -m 1 "IP:" | sed -e 's/[^0-9.]//g' | tr '.' '-' | sed 's/$/.default.pod/')
+PODIP=$($k describe po nginx-resolver | grep -i -m 1 "IP:" | sed -e 's/[^0-9.]//g' | tr '.' '-' | sed 's/$/.default.pod/')
 
 $k run test-nslookup --image=busybox:1.28 -it -- nslookup nginx-resolver-service > /root/nginx.svc
 
-$k run test-nslookup --image=busybox:1.28 -it -- nslookup $PODIP > /root/nginx.pod
+$k run test-nslookup-pod --image=busybox:1.28 -it -- nslookup $PODIP > /root/nginx.pod
